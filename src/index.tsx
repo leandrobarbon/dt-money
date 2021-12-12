1737b81e74442464ae19e7ad5251127e3bf3d2bd
Leandro da silva <leandrosilva.hair@gmail.com>
2021-12-12 14:57:36 -0300

    Consumindo API

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer de web',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Prestação da casa',
+          type: 'withdraw',
+          category: 'Contas casa',
+          amount: 900,
+          createAt: new Date('2021-12-13 09:00:00')
+        },
+      ],
+    })
+  },
+
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: "Transaction 1",
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createAt: new Date(),
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => { 
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data);
     })
   }
 })
